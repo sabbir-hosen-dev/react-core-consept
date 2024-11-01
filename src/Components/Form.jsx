fbe068fed3e2e01b0a8a103cc1648c1e9184bc93
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2024-11-01 20:57:58 +0600

    check

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -4,8 +4,8 @@ function Form() {
   
   const handleSubmit = (e) => {
     e.preventDefault(); 
-    console.log(e.target.name.value)
-    console.log(e.target.email.value)
+    console.log(e.target.name.value);
+    console.log(e.target.email.value);
   }
   return (
     <form onSubmit={handleSubmit}>
